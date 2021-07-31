49bc83ff7f98faae5031b5b42fbd51bb7931673d
gemmaro <gemmaro.dev@gmail.com>
2021-07-31 10:48:19 +0900

    [ add ] Copy from local

--- src/make.cr
+++ src/make.cr
@@ -1,6 +1,92 @@
-# TODO: Write documentation for `Make`
 module Make
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class Make
+    def initialize
+      @tasks = Tasks.new
+      yield @tasks
+    end
+
+    def run(paths : Array(Path | Array(Path)))
+      paths.flatten.each do |path|
+        @tasks.run(path)
+      end
+    end
+  end
+
+  class Flow
+    getter target : Path
+    getter sources : Array(Path)
+
+    def initialize(@target : Path, @sources : Array(Path))
+    end
+
+    def source : Path
+      @sources[0]
+    end
+  end
+
+  class Tasks
+    def initialize
+      @directories = Set(Path).new
+      @files = Hash(Path, Tuple(Array(Path), Proc(Flow, Nil))).new
+    end
+
+    def directory(path : Path)
+      @directories << path
+    end
+
+    def file(path : Path, source : Path, &action : Flow ->)
+      file(path, [source], action)
+    end
+
+    def file(path : Path, sources : Array(Path | String), &action : Flow ->)
+      file(path, sources, action)
+    end
+
+    def file(path : Path, sources : Array(Path | String), action : Proc(Flow, Nil))
+      sources = sources.map { |source| Path.new(source) }
+      @files[path] = {sources, action}
+    end
+
+    def namespace(_name : Symbol)
+      yield
+    end
+
+    protected def run(path : Path)
+      if @directories.includes?(path)
+        mkdir(path)
+      elsif @files.has_key?(path)
+        sources, action = @files[path]
+
+        sources.each do |source|
+          run(source)
+        end
+
+        generate(path, sources, action)
+      else
+        generate(path)
+      end
+    end
+
+    private def mkdir(path : Path)
+      generate(path, Array(Path).new) do |s|
+        FileUtils.mkdir(s.target)
+      end
+    end
+
+    private def generate(path : Path)
+      raise "#{path} が存在しません。" unless File.exists?(path)
+    end
+
+    private def generate(path : Path, sources : Array(Path), &block : Flow ->)
+      generate(path, sources, block)
+    end
+
+    private def generate(path : Path, sources : Array(Path), action)
+      return if File.exists?(path)
+      action.call(Flow.new(path, sources))
+      raise "#{path} は生成されませんでした。" unless File.exists?(path)
+    end
+  end
 end
